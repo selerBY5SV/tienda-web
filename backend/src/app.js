1e14ea345219b551ce400e772f4a041331b32cf7
selerBY5SV <a29773@svalero.com>
2026-04-30 00:30:24 +0200

    Servidor Express funcionando correctamente

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API funcionando correctamente');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
